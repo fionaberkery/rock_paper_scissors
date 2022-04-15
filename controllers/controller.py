001ec7c26a60e353b982728ed6b08d4d742165ad
Fiona <fiona_berkery@hotmail.com>
2022-04-15 13:39:58 +0100

    MVP completed

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,9 +1,16 @@
-from code import interact
-from flask import render_template # ADDED
+
+from flask import render_template
 from app import app
-from models.game import Game
-from models.player import Player
+# from models.player import *
+from models.game import *
+
 
 @app.route('/')
 def index():
     return render_template('index.html', title='Home')
+
+@app.route('/<player1_choice>/<player2_choice>')
+def game_page(player1_choice, player2_choice):
+    game = Game()
+    winner = game.play_game(player1_choice, player2_choice)
+    return render_template('result.html', title='Game', winner=winner)
--- models/game.py
+++ models/game.py
@@ -1,5 +1,28 @@
 class Game():
-    def __init__(self, name):
-        self.name = name
 
-    
+
+    def play_game(self, player1_choice, player2_choice):                     
+        if player1_choice == "scissors" and player2_choice == "rock":
+            return "Player 2 wins by playing rock"
+
+        elif player1_choice == "scissors" and player2_choice == "paper":
+            return "Player 1 wins by playing scissors" 
+
+        elif player1_choice == "rock" and player2_choice == "scissors":
+            return "Player 1 wins by playing rock"
+            
+        elif player1_choice == "rock" and player2_choice == "paper":
+            return "Player 1 wins by playing paper"
+
+        elif player1_choice == "paper" and player2_choice == "scissor":
+            return "Player 2 wins by playing scissor"
+        
+        elif player1_choice == "paper" and player2_choice == "rock":
+            return "Player 2 wins by playing rock"
+
+        else:
+            player1_choice == player2_choice
+            return "Its a draw"
+
+
+            
