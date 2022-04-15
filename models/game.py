d8e269a5d325f83814814243db93568cb093c117
Fiona <fiona_berkery@hotmail.com>
2022-04-15 13:47:47 +0100

    spelling error in string corrected

--- models/game.py
+++ models/game.py
@@ -12,13 +12,13 @@ class Game():
             return "Player 1 wins by playing rock"
             
         elif player1_choice == "rock" and player2_choice == "paper":
-            return "Player 1 wins by playing paper"
+            return "Player 2 wins by playing paper"
 
         elif player1_choice == "paper" and player2_choice == "scissor":
             return "Player 2 wins by playing scissor"
         
         elif player1_choice == "paper" and player2_choice == "rock":
-            return "Player 2 wins by playing rock"
+            return "Player 1 wins by playing paper"
 
         else:
             player1_choice == player2_choice
